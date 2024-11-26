a2ae0a4d7bbe0bd6b2df75425d3c1eaa7e6e40a5
jdsteege <jdsteege@gmail.com>
2024-11-26 13:28:11 -0600

    Firebase and VueFire setup

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,15 @@ import { createPinia } from "pinia";
 import PrimeVue from "primevue/config";
 import Aura from "@primevue/themes/aura";
 
+import { VueFire, VueFireAppCheck } from "vuefire";
+import { ReCaptchaV3Provider } from "firebase/app-check";
+
 import App from "./App.vue";
 import router from "./router";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 const app = createApp(App);
 
@@ -16,13 +23,38 @@ app.use(PrimeVue, {
   theme: {
     preset: Aura,
     options: {
-      darkModeSelector: '.dark',
+      darkModeSelector: ".dark",
       cssLayer: {
         name: "primevue",
         order: "tailwind-base, primevue, tailwind-utilities",
       },
     },
   },
+});
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOt-VQMVFirNGzdbfnmnBOmMDtoiyKQA8",
+  authDomain: "saving-jar-coop.firebaseapp.com",
+  projectId: "saving-jar-coop",
+  storageBucket: "saving-jar-coop.firebasestorage.app",
+  messagingSenderId: "341615997866",
+  appId: "1:341615997866:web:f5dd67e006a2479efd71d3",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getDatabase(firebaseApp);
+
+app.use(VueFire, {
+  firebaseApp,
+  modules: [
+    VueFireAppCheck({
+      provider: new ReCaptchaV3Provider("6LfWA4sqAAAAAFxZcQKn8TRhlJWhZHvrpLMmpxRN"),
+      debug: true,
+      isTokenAutoRefreshEnabled: true,
+    }),
+  ],
 });
 
 app.mount("#app");
--- src/views/JarsListView.vue
+++ src/views/JarsListView.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
+import {
+  getDatabase,
+  ref as dbRef,
+  type DatabaseReference,
+} from "firebase/database";
+import { useDatabaseList } from "vuefire";
 
+const db = getDatabase();
+const jarsRef = useDatabaseList(dbRef(db, "jars"));
 </script>
 
 <template>
-    <div>Jars List</div>
+  <ul>
+    <li v-for="jar in jarsRef" :key="jar.id">
+      <span> {{ jar.id }}</span>
+    </li>
+  </ul>
 </template>
