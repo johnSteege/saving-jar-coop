9c4d516ed04eb8bc9e3a0d753856c97ae45d9a5b
jdsteege <jdsteege@gmail.com>
2024-12-12 09:31:48 -0600

    Transactions list placeholder

--- src/views/TransactionsListView.vue
+++ src/views/TransactionsListView.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
+import { transactionsRef, type Transaction } from "@/data/firebaseHelpers";
+import { useDatabaseList } from "vuefire";
 
+const transactionsList = useDatabaseList<Transaction>(transactionsRef);
 </script>
 
 <template>
-    <div>Transactions List</div>
+  <ul>
+    <li v-for="transaction in transactionsList" :key="transaction.id">
+      <div>{{ transaction.id }}</div>
+      <div>{{ transaction }}</div>
+    </li>
+  </ul>
 </template>
